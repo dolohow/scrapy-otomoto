e5fca3a04db5be7bb00e47b75959d026eedf499a
aulemasa <maszs@wp.pl>
2020-03-09 19:55:25 +0100

    Fix crawler hangup on startup
    By enabling UserAgentMiddleware crawler starts to work correctly again.
    
    Reviewed-by: Łukasz Żarnowiecki <lukasz@zarnowiecki.pl>

--- otomoto/settings.py
+++ otomoto/settings.py
@@ -52,9 +52,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None
 #    'otomoto.middlewares.OtomotoDownloaderMiddleware': 543,
-#}
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
